3bbfcfcf1784c514e4b706ca124b64069298cae8
seyistry <seyistry@gmail.com>
2023-04-20 14:18:35 +0100

    format code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""
+pascal_triangle interview
+"""
 from math import factorial
 
+
 def pascal_triangle(n):
-	outer_list = []
-	if n <= 0:
-		return outer_list
-	for i in range( 1, n + 1):
-		inner_list = []
-		for j in range(i + 1):
-			inner_list.append(int(factorial(i)/(factorial(j) * factorial(i - j))))
-		outer_list.append(inner_list)
-	return outer_list
+    outer_list = []
+    if n <= 0:
+        return outer_list
+    for i in range(1, n + 1):
+        inner_list = []
+        for j in range(i + 1):
+            inner_list.append(
+                int(factorial(i)/(factorial(j) * factorial(i - j))))
+        outer_list.append(inner_list)
+    return outer_list
