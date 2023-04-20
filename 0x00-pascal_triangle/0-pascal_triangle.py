d72e292beab5e8784426dec2b0010f28453a7952
seyistry <seyistry@gmail.com>
2023-04-20 18:15:28 +0100

    print empty arr if n < 1

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -31,7 +31,7 @@ def pascal_triangle(n):
     """
     outer_list = []
     if n <= 0:
-        return outer_list
+        return [outer_list]
     for i in range(n):
         inner_list = []
         for j in range(i + 1):
