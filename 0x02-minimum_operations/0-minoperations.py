a37393395035a6e81180a30c57298bfb78622df9
seyistry <seyistry@gmail.com>
2023-05-04 20:13:18 +0100

    add minimum operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+# This prime factor is from GeekofGeek
+# and contributed by Taranpreet.
+def primeFactors(n):
+    """calculate prime factors
+
+    Args:
+        n (int): int
+
+    Returns:
+        list: return a factors of int
+    """
+    arr = []
+    c = 2
+    while (n > 1):
+        if (n % c == 0):
+            arr.append(c)
+            n = n / c
+        else:
+            c = c + 1
+    return arr
+
+
+
+def minOperations(n):
+    """ method that calculates
+        the fewest number of operations 
+        needed to result in exactly n H 
+        characters in the file.
+
+    Args:
+        n (int): minimum operation
+
+    Returns:
+        int: return an integer
+    """
+    sum = 0
+    for i in primeFactors(n):
+        sum = sum + i
+    return sum
