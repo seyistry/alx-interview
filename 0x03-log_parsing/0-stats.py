17b2f4bff51e282f2d5ccf16d2a2d24b85f820bc
seyistry <seyistry@gmail.com>
2023-05-12 01:09:11 +0100

    use sys lib

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,7 +2,6 @@
 """0x03. Log Parsing
     """
 
-import fileinput
 import sys
 import os
 
@@ -11,33 +10,31 @@ add_status_call = {}
 count = 0
 file_size = 0
 
-if __name__ == '__main__':
-    try:
-        for fileinput_line in fileinput.input():
-            count = count + 1
-            arr = str(fileinput_line).strip().split(" ")
-            file_size = file_size + int(arr[-1])
-            # print(arr[-2])
-            try:
-                if int(arr[-2]) in allowed_status_code:
-                    if int(arr[-2]) not in add_status_call:
-                        add_status_call[int(arr[-2])] = 1
-                    else:
-                        add_status_call[int(
-                            arr[-2])] = add_status_call[int(arr[-2])] + 1
-            except TypeError():
-                continue
-            if count == 10:
-                print("File size: {}".format(file_size))
-                for key in sorted(add_status_call):
-                    print("{}: {}".format(key, add_status_call[key]))
-                count = 0
-                file_size = 0
-    except KeyboardInterrupt:
-        print("File size: {}".format(file_size))
-        for key in sorted(add_status_call):
-            print("{}: {}".format(key, add_status_call[key]))
+try:
+    for line in sys.stdin:
+        count = count + 1
+        arr = str(line).strip().split(" ")
+        file_size = file_size + int(arr[-1])
         try:
-            sys.exit(130)
-        except SystemExit:
-            os._exit(130)
+            if int(arr[-2]) in allowed_status_code:
+                if int(arr[-2]) not in add_status_call:
+                    add_status_call[int(arr[-2])] = 1
+                else:
+                    add_status_call[int(
+                        arr[-2])] = add_status_call[int(arr[-2])] + 1
+        except TypeError():
+            continue
+        if count == 10:
+            print("File size: {}".format(file_size))
+            for key in sorted(add_status_call):
+                print("{}: {}".format(key, add_status_call[key]))
+            count = 0
+            file_size = 0
+except KeyboardInterrupt:
+    print("File size: {}".format(file_size))
+    for key in sorted(add_status_call):
+        print("{}: {}".format(key, add_status_call[key]))
+    try:
+        sys.exit(1)
+    except SystemExit:
+        os._exit(1)
