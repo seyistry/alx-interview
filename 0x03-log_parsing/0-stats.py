bd36a943ab704a29b7b35a8cc9491203e2d79531
seyistry <seyistry@gmail.com>
2023-05-14 19:25:50 +0100

    use try except

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -10,22 +10,27 @@ add_status_call = {}
 count = 0
 file_size = 0
 
-for line in sys.stdin:
-    count = count + 1
-    arr = str(line).strip().split(" ")
-    file_size = file_size + int(arr[-1])
-    try:
-        if int(arr[-2]) in allowed_status_code:
-            if int(arr[-2]) not in add_status_call:
-                add_status_call[int(arr[-2])] = 1
-            else:
-                add_status_call[int(
-                    arr[-2])] = add_status_call[int(arr[-2])] + 1
-    except TypeError():
-        continue
-    if count == 10:
-        print("File size: {}".format(file_size))
-        for key in sorted(add_status_call):
-            print("{}: {}".format(key, add_status_call[key]))
-        count = 0
-        file_size = 0
+try:
+    for line in sys.stdin:
+        count = count + 1
+        arr = str(line).strip().split(" ")
+        file_size = file_size + int(arr[-1])
+        try:
+            if int(arr[-2]) in allowed_status_code:
+                if int(arr[-2]) not in add_status_call:
+                    add_status_call[int(arr[-2])] = 1
+                else:
+                    add_status_call[int(
+                        arr[-2])] = add_status_call[int(arr[-2])] + 1
+        except TypeError():
+            continue
+        if count == 10:
+            print("File size: {}".format(file_size))
+            for key in sorted(add_status_call):
+                print("{}: {}".format(key, add_status_call[key]))
+            count = 0
+            file_size = 0
+except KeyboardInterrupt:
+    print("File size: {}".format(file_size))
+    for key in sorted(add_status_call):
+        print("{}: {}".format(key, add_status_call[key]))
