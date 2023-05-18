c9687fb5be22ba4dd082d499e9e4f2714bf2e9d4
seyistry <seyistry@gmail.com>
2023-05-18 22:32:59 +0100

    increase range

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,17 +6,17 @@ Validate if list of int are all valid UTF-8
 
 def validUTF8(data):
     for i in data:
-        if i in range(1, 128):
+        if i in range(1, 256):
             a = bool(i & (1 << (8 - 1)))
             if a is True:
                 return False
-        elif i in range(128, 2048):
+        elif i in range(256, 65536):
             a = bool(i & (1 << (8 - 1)))
             b = bool(i & (1 << (15 - 1)))
             c = bool(i & (1 << (16 - 1)))
             if (a and b and c) is not True:
                 return False
-        elif i in range(2048, 65536):
+        elif i in range(65536, 16777216):
             a = bool(i & (1 << (8 - 1)))
             b = bool(i & (1 << (16 - 1)))
             c = bool(i & (1 << (22 - 1)))
@@ -24,7 +24,7 @@ def validUTF8(data):
             e = bool(i & (1 << (24 - 1)))
             if (a and b and c and d and e) is not True:
                 return False
-        elif i in range(65536, 1114111):
+        elif i in range(16777216, 4294967296):
             a = bool(i & (1 << (8 - 1)))
             b = bool(i & (1 << (16 - 1)))
             c = bool(i & (1 << (24 - 1)))
