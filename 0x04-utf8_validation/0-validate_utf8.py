aefd58415ed781ccc9699193eb4dcd7e857c1593
seyistry <seyistry@gmail.com>
2023-05-18 21:57:14 +0100

    using 8 lsb

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,6 +6,8 @@ Validate if list of int are all valid UTF-8
 
 def validUTF8(data):
     for i in data:
-        if i > 127:
+        x = bool(i & (1 << (8 - 1) ))
+        if x == True:
             return False
     return True
+    
