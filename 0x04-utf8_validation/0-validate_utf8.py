41a658abf67d4db953613ced035de58b68e5cb45
seyistry <seyistry@gmail.com>
2023-05-18 21:42:38 +0100

    fix code style error

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,8 +3,9 @@
 Validate if list of int are all valid UTF-8
 """
 
+
 def validUTF8(data):
-	for i in data:
-		if i > 127:
-			return False
-	return True
+    for i in data:
+        if i > 127:
+            return False
+    return True
