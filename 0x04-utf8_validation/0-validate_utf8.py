1b0a3b05a14728e1ea98a2b9b99bd3ea7f03f15b
seyistry <seyistry@gmail.com>
2023-05-18 22:20:29 +0100

    optimize check for higher utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,7 +6,35 @@ Validate if list of int are all valid UTF-8
 
 def validUTF8(data):
     for i in data:
-        x = bool(i & (1 << (8 - 1)))
-        if x is True:
+        if i in range(1, 128):
+            a = bool(i & (1 << (8 - 1)))
+            if a is True:
+                return False
+        elif i in range(128, 2048):
+            a = bool(i & (1 << (8 - 1)))
+            b = bool(i & (1 << (15 - 1)))
+            c = bool(i & (1 << (16 - 1)))
+            if (a and b and c) is not True:
+                return False
+        elif i in range(2048, 65536):
+            a = bool(i & (1 << (8 - 1)))
+            b = bool(i & (1 << (16 - 1)))
+            c = bool(i & (1 << (22 - 1)))
+            d = bool(i & (1 << (23 - 1)))
+            e = bool(i & (1 << (24 - 1)))
+            if (a and b and c and d and e) is not True:
+                return False
+        elif i in range(65536, 1114111):
+            a = bool(i & (1 << (8 - 1)))
+            b = bool(i & (1 << (16 - 1)))
+            c = bool(i & (1 << (24 - 1)))
+            d = bool(i & (1 << (29 - 1)))
+            e = bool(i & (1 << (30 - 1)))
+            f = bool(i & (1 << (31 - 1)))
+            g = bool(i & (1 << (32 - 1)))
+            if (a and b and c and d and e and f and g) is not True:
+                return False
+        else:
             return False
+
     return True
