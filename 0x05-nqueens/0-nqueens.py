3aa9513ba568cf33d007ccdd98c67af8a48a1103
seyistry <seyistry@gmail.com>
2023-05-31 18:49:35 +0100

    check file name

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -10,20 +10,20 @@ def queens(n, i, a, b, c):
     else:
         yield a
 
-
-if (len(sys.argv) == 2):
-    try:
-        if (int(sys.argv[1]) >= 4):
-            for solution in queens(int(sys.argv[1]), 0, [], [], []):
-                for item in range(len(solution)):
-                    solution[item] = [item, solution[item]]
-                print(solution)
-        else:
-            print("N must be at least 4\n")
+if __name__ == '__main__':
+    if (len(sys.argv) == 2):
+        try:
+            if (int(sys.argv[1]) >= 4):
+                for solution in queens(int(sys.argv[1]), 0, [], [], []):
+                    for item in range(len(solution)):
+                        solution[item] = [item, solution[item]]
+                    print(solution)
+            else:
+                print("N must be at least 4\n")
+                sys.exit(1)
+        except ValueError as e:
+            print("N must be a number\n")
             sys.exit(1)
-    except ValueError as e:
-        print("N must be a number\n")
+    else:
+        print("Usage: nqueens N\n")
         sys.exit(1)
-else:
-    print("Usage: nqueens N\n")
-    sys.exit(1)
